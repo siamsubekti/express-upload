fcff5ab793b284cde016e310f8719e5fcb36698f
tienbku <tien.ngtruong@gmail.com>
2022-08-02 10:14:19 +0700

    add file delete

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -64,8 +64,44 @@ const download = (req, res) => {
   });
 };
 
+const remove = (req, res) => {
+  const fileName = req.params.name;
+  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+  fs.unlink(directoryPath + fileName, (err) => {
+    if (err) {
+      res.status(500).send({
+        message: "Could not delete the file. " + err,
+      });
+    }
+
+    res.status(200).send({
+      message: "File is deleted.",
+    });
+  });
+};
+
+const removeSync = (req, res) => {
+  const fileName = req.params.name;
+  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+
+  try {
+    fs.unlinkSync(directoryPath + fileName);
+
+    res.status(200).send({
+      message: "File is deleted.",
+    });
+  } catch (err) {
+    res.status(500).send({
+      message: "Could not delete the file. " + err,
+    });
+  }
+};
+
 module.exports = {
   upload,
   getListFiles,
   download,
+  remove,
+  removeSync,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ let routes = (app) => {
   router.post("/upload", controller.upload);
   router.get("/files", controller.getListFiles);
   router.get("/files/:name", controller.download);
+  router.delete("/files/:name", controller.remove);
 
   app.use(router);
 };
